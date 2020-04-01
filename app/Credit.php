279d98051f7d33237e5c58739565a4769bdb6b02
Edgar Rosas <gary.blurk@gmail.com>
2020-04-01 16:50:02 -0700

    credit model and request

--- app/Credit.php
+++ app/Credit.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Credit extends Model
 {
-    //
+    public $fillable = [
+        'amount_due',
+        'series',
+        'number',
+        'comments',
+        'original_amount_due',
+        'concept',
+        'date',
+        'invoice_id',
+    ];
+
+    protected $dates = ['date', 'created_at', 'updated_at'];
+
+    public function getAmountDueAttribute($value)
+    {
+        return number_format($value, 4);
+    }
+
+    public function getOriginalAmountDueAttribute($value)
+    {
+        return number_format($value, 4);
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo('App\Invoice');
+    }
 }
