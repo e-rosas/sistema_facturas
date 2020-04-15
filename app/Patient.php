a5129096ec49410aa0929e27f8477e1a6c61ae06
Edgar Rosas <gary.blurk@gmail.com>
2020-04-15 10:20:06 -0700

    patient created event

--- app/Patient.php
+++ app/Patient.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use App\Events\PatientCreated;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Patient extends Model
 {
+    use Notifiable;
     public $fillable = [
         'last_name',
         'maiden_name',
@@ -21,6 +24,14 @@ class Patient extends Model
         'insurance_id',
         'insurer_id',
     ];
+    /**
+     * The event map for the model.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'saved' => PatientCreated::class,
+    ];
     protected $dates = ['birth_date'];
 
     public function invoices()
@@ -28,19 +39,15 @@ class Patient extends Model
         return $this->hasMany('App\Invoice');
     }
 
-    
     public function insurer()
     {
         return $this->belongsTo('App\Insurer');
     }
 
-    
     public function person_stats()
     {
         return $this->hasOne('App\PersonStats');
     }
-    
-    
 
     public function addressDetails()
     {
