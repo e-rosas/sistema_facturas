c5b8a6690c84468b8b087ce9ec6970bc6883d4dd
Edgar Rosas <gary.blurk@gmail.com>
2020-05-12 16:51:08 -0700

    charges and letters

---
+++ database/migrations/2020_05_12_225337_create_charges_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChargesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('charges', function (Blueprint $table) {
+            $table->unsignedBigInteger('invoice_id')->primary();
+            $table->decimal('original_amount_due', 13, 4);
+            $table->decimal('amount_charged', 13, 4);
+            $table->decimal('exchange_rate', 13, 4);
+            $table->date('date');
+            $table->tinyInteger('status')->default(0);
+            $table->text('comments')->nullable();
+            $table->foreign('invoice_id')->references('id')->on('invoices')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('charges');
+    }
+}
---
+++ database/migrations/2020_05_12_232059_create_letters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('letters', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('charge_id');
+            $table->date('date');
+            $table->string('file_path')->nullable();
+            $table->text('comments')->nullable();
+            $table->foreign('charge_id')->references('invoice_id')->on('charges')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('letters');
+    }
+}
