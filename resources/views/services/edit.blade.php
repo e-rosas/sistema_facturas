303d4a05dbb8f30633e7ebd814006968305a5432
HM Edgar <hospmex.sistemas@gmail.com>
2020-06-26 09:01:38 -0700

    edit service

--- resources/views/services/edit.blade.php
+++ resources/views/services/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['title' => 'Servicios'])
 
 @section('content')
-    @include('layouts.headers.header', ['title' => 'Editar servicio'])   
+    @include('layouts.headers.header', ['title' => 'Editar servicio'])
 
     <div class="container-fluid mt--7">
         <div class="row">
@@ -30,23 +30,23 @@
                                     {{--  Code --}}
                                     <div class="col-md-2 col-auto form-group{{ $errors->has('code') ? ' has-danger' : '' }}">
                                         <label class="form-control-label" for="input-code">Código</label>
-                                        <input type="text" name="code" id="input-code" class="form-control form-control-alternative{{ $errors->has('code') ? ' is-invalid' : '' }}" 
+                                        <input type="text" name="code" id="input-code" class="form-control form-control-alternative{{ $errors->has('code') ? ' is-invalid' : '' }}"
                                         placeholder="Código" value="{{ $service->code }}">
-                                    
+
                                         @if ($errors->has('code'))
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $errors->first('code') }}</strong>
                                             </span>
                                         @endif
                                     </div>
-                                    
+
                                 </div>
                                 {{--  Description  --}}
                                 <div class="form-group{{ $errors->has('description') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-description">Description</label>
-                                    <input type="text" name="description" id="input-description" class="form-control form-control-alternative{{ $errors->has('description') ? ' is-invalid' : '' }}" 
+                                    <input type="text" name="description" id="input-description" class="form-control form-control-alternative{{ $errors->has('description') ? ' is-invalid' : '' }}"
                                     placeholder="Descripción" value="{{ $service->description }}" required>
-                                
+
                                     @if ($errors->has('description'))
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $errors->first('description') }}</strong>
@@ -56,7 +56,7 @@
                                 {{--  Descripcion  --}}
                                 <div class="form-group{{ $errors->has('descripcion') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-descripcion">Descripción</label>
-                                    <input type="text" name="descripcion" id="input-descripcion" class="form-control form-control-alternative{{ $errors->has('descripcion') ? ' is-invalid' : '' }}" 
+                                    <input type="text" name="descripcion" id="input-descripcion" class="form-control form-control-alternative{{ $errors->has('descripcion') ? ' is-invalid' : '' }}"
                                     placeholder="Descripción" value="{{ $service->descripcion }}" required>
 
                                     @if ($errors->has('descripcion'))
@@ -69,8 +69,8 @@
                                     {{--  SAT --}}
                                     <div class="col-md-6 col-auto form-group{{ $errors->has('SAT') ? ' has-danger' : '' }}">
                                         <label class="form-control-label" for="input-SAT">Código SAT (opcional)</label>
-                                        <input type="text" name="SAT" id="input-SAT" class="form-control form-control-alternative{{ $errors->has('SAT') ? ' is-invalid' : '' }}" 
-                                        placeholder="Código" value="{{ $service->SAT }}">
+                                        <input type="text" name="SAT" id="input-SAT" class="form-control form-control-alternative{{ $errors->has('SAT') ? ' is-invalid' : '' }}"
+                                        placeholder="Código" value="{{ $service->SAT ?? 'Pendiente' }}">
 
                                         @if ($errors->has('SAT'))
                                             <span class="invalid-feedback" role="alert">
@@ -81,7 +81,7 @@
                                     {{--  SAT_code --}}
                                     <div class="col-md-6 col-auto form-group{{ $errors->has('SAT_code') ? ' has-danger' : '' }}">
                                         <label class="form-control-label" for="input-SAT_code">Código SAT (interno)</label>
-                                        <input type="text" name="SAT_code" id="input-SAT_code" class="form-control form-control-alternative{{ $errors->has('SAT_code') ? ' is-invalid' : '' }}" 
+                                        <input type="text" name="SAT_code" id="input-SAT_code" class="form-control form-control-alternative{{ $errors->has('SAT_code') ? ' is-invalid' : '' }}"
                                         placeholder="Código" value="{{ $service->SAT_code }}" required>
 
                                         @if ($errors->has('SAT_code'))
@@ -92,13 +92,13 @@
                                     </div>
                                 </div>
                                 <div class="form-row">
-                                    
+
                                     {{--  total_price  --}}
                                     <div class="col-md-4 col-auto form-group{{ $errors->has('price') ? ' has-danger' : '' }}">
                                         <label class="form-control-label" for="input-total_price">Precio total</label>
-                                        <input type="numeric" name="price" id="input-price" class="form-control form-control-alternative{{ $errors->has('price') ? ' is-invalid' : '' }}" 
+                                        <input type="numeric" name="price" id="input-price" class="form-control form-control-alternative{{ $errors->has('price') ? ' is-invalid' : '' }}"
                                         placeholder="Precio total" value="{{ $service->price }}" required>
-                                    
+
                                         @if ($errors->has('price'))
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $errors->first('price') }}</strong>
@@ -108,9 +108,9 @@
                                     {{--  Discount  --}}
                                     <div class="col-md-4 col-auto form-group{{ $errors->has('discounted_price') ? ' has-danger' : '' }}">
                                         <label class="form-control-label" for="input-discounted_price">Precio con descuento</label>
-                                        <input type="numeric" name="discounted_price" id="input-discounted_price" class="form-control form-control-alternative{{ $errors->has('discounted_price') ? ' is-invalid' : '' }}" 
+                                        <input type="numeric" name="discounted_price" id="input-discounted_price" class="form-control form-control-alternative{{ $errors->has('discounted_price') ? ' is-invalid' : '' }}"
                                         placeholder="Precio con descuento" value="{{ $service->discounted_price }}" required>
-                                    
+
                                         @if ($errors->has('discounted_price'))
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $errors->first('discounted_price') }}</strong>
@@ -124,7 +124,7 @@
                                         @foreach($categories as $category)
                                             <option value="{{ $category->id }}" {{ $service->category_id == $category->id ? 'selected' : '' }}>{{ $category->nombre }}</option>
                                         @endforeach
-                                        </select>                  
+                                        </select>
                                         @if ($errors)
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $errors->first('category_id') }}</strong>
@@ -141,7 +141,7 @@
                 </div>
             </div>
         </div>
-        
+
         @include('layouts.footers.auth')
     </div>
 @endsection
