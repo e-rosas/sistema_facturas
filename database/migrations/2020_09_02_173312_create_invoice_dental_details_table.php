5c0db2ba572aaef09be3481fd33f8a8d9665ee59
HM Edgar <hospmex.sistemas@gmail.com>
2020-09-03 13:02:27 -0700

    invoice dental details migration, model

---
+++ database/migrations/2020_09_02_173312_create_invoice_dental_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceDentalDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('invoice_dental_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('invoice_id')->primary();
+            $table->boolean('enclosures')->default(0);
+            $table->boolean('orthodontics')->default(0);
+            $table->date('appliance_placed');
+            $table->tinyInteger('months_remaining')->default(0);
+            $table->boolean('prosthesis_replacement')->default(0);
+            $table->tinyInteger('treatment_resulting_from')->default(0);
+
+            $table->date('prior_placement');
+
+            $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_dental_details');
+    }
+}
